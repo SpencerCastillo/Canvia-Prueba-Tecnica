107ba11fe2c2ad5bc95809012640817157e6cef8
SpencerCastillo <73484307+SpencerCastillo@users.noreply.github.com>
2022-06-15 16:30:04 -0500

    Add files via upload

---
+++ PRUEBA-API/PRUEBAImplementacion/Cliente/ClienteRequest.cs
@@ -0,0 +1,42 @@
+﻿using PRUEBAEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRUEBAImplementacion.Cliente
+{
+    public class ClienteRequest
+    {
+        public  BE_MAE_CLIENTE Cliente { get; set; }
+    }
+
+    public class ClienteResponse
+    {
+        public BE_MAE_CLIENTE Cliente { get; set; }
+        public List<BE_MAE_CLIENTE> Clientes { get; set; }
+        public int Total { get; set; }
+        public string vcMensaje { get; set; }
+        public string vcStatus { get; set; }
+        public int inCodigoRespuesta { get; set; }
+        public int inIdGenerado { get; set; }
+    }
+
+    public class FilterCliente
+    {
+        public string vcNumeroDocumento { set; get; }
+
+        public string nvNombres { set; get; }
+        public string vcEstado { set; get; }
+
+        public int pageIndex { set; get; }
+
+        public int pageSize { set; get; }
+
+        public int inCodUsuario_Aud { get; set; }
+
+        public bool biNoEliminadoLogicoBD { set; get; }
+    }
+    
+}
---
+++ PRUEBA-API/PRUEBAImplementacion/Item/ItemRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PRUEBAEntity;
+namespace PRUEBAImplementacion.Item
+{
+    public class ItemRequest
+    {
+        public BE_MAE_ITEM  Item { get; set; }
+
+    }
+
+    public class ItemResponse
+    {
+        public BE_MAE_ITEM Item { get; set; }
+        public List<BE_MAE_ITEM> Items { get; set; }
+        public int Total { get; set; }
+        public string vcMensaje { get; set; }
+        public string vcStatus { get; set; }
+        public int inCodigoRespuesta { get; set; }
+        public int inIdGenerado { get; set; }
+    }
+
+    public class FilterItem
+    {
+        public string vcDescripcionItem { set; get; }
+        public int pageIndex { set; get; }
+
+        public int pageSize { set; get; }
+
+        public int inCodUsuario_Aud { get; set; }
+
+        public bool biNoEliminadoLogicoBD { set; get; }
+    }
+}
